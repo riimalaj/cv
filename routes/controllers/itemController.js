f4163a176ab975aff5b6afe4e0d8ac0f9e93add1
riimalaj <jriimala@gmail.com>
2023-01-02 07:13:23 +0200

    Attr loc.ip empty check added prior loc upd to db.

--- routes/controllers/itemController.js
+++ routes/controllers/itemController.js
@@ -63,7 +63,7 @@ const paivitaNettiData = async () => {
     let dupCheckResp = await itemServices.checkDup(loc.ip);
     console.log('dupCheckResp:', dupCheckResp);
 
-    if (dupCheckResp < 1  && loc.continent_name != null) {
+    if (dupCheckResp < 1  && (loc.continent_name != null && loc.ip != "")) {
         await itemServices.updateTracker(loc);
     } else {
         console.log("Allready there, so let's not duplicate.");
